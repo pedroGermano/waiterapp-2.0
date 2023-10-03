9acc59644c5b35ef418db375633d7fa146317e53
Pedro Germano <pedrogermano232@gmail.com>
2023-10-02 22:14:55 -0300

    mongo ok

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => console.log('conectado ao mongo'))
+  .catch(() => console.log('Error ao conectar'));
 
 const port = 3001;
 app.listen(3001, () => {
