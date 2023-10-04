cee657eb1670421d4cb3efa9b793a2c30f4eea20
Pedro Germano <pedrogermano232@gmail.com>
2023-10-04 19:40:13 -0300

    ok ok

--- api/src/app/useCases/categories/listCategories.ts
+++ api/src/app/useCases/categories/listCategories.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function listCategories(req: Request, res: Response) {
 
 }
