b8334fd0a9544594c5fd0dd0eaa412075676d9d5
Pedro Germano <pedrogermano232@gmail.com>
2023-10-05 20:08:45 -0300

    ok

--- api/src/app/useCases/categories/createCategory.ts
+++ api/src/app/useCases/categories/createCategory.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 //import { Category } from '../../models/Category';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function createCategory(req: Request, res: Response) {
 
 }
